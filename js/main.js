3a771e5cd2ffc63d759b8bccc9887e971b928f0f
pgorn <pgornisi@student.wszib.edu.pl>
2023-07-05 18:02:10 +0200

    wersja 0.1

--- js/main.js
+++ js/main.js
@@ -1,21 +1,42 @@
 function frezowanie(){
   const xhttp = new XMLHttpRequest();
   xhttp.onload = function() {
-    document.getElementById("demo2").innerHTML = "";
-    document.getElementById("demo3").innerHTML = "";
-    document.getElementById("demo").innerHTML = this.responseText;
+    document.getElementById("wynik_obr").innerHTML = "";
+    document.getElementById("wynik_pos").innerHTML = "";
+    document.getElementById("ankieta").innerHTML = this.responseText;
   }
   xhttp.open("GET", "frezowanie.html", true);
   xhttp.send();
 
 }
 
+function frezowanie_param(){
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    let mill_mat = document.getElementById("mill_material").value;
+    let mill_tool_material = document.getElementById("mill_tool_material").value;
+    let mill_tool_diameter = document.getElementById("mill_tool_diameter").value;
+    let mill_tool_cutting_edges = document.getElementById("mill_tool_cutting_edges").value;
+    let obroty;
+    let posuw;
+    obroty = (1000*(mill_mat*mill_tool_material))/(Math.PI*mill_tool_diameter);
+    posuw = (obroty*mill_tool_cutting_edges)/10;
+    document.getElementById("wynik_obr").innerHTML = Math.round(obroty) + " obr/min";
+    document.getElementById("wynik_pos").innerHTML = Math.round(posuw) + " mm/min";
+  }
+  xhttp.open("GET", "frezowanie.html", true);
+  xhttp.send();
+
+}
+
+
+
 function wiercenie(){
   const xhttp = new XMLHttpRequest();
   xhttp.onload = function() {
-    document.getElementById("demo2").innerHTML = "";
-    document.getElementById("demo3").innerHTML = "";
-    document.getElementById("demo").innerHTML = this.responseText;
+    document.getElementById("wynik_obr").innerHTML = "";
+    document.getElementById("wynik_pos").innerHTML = "";
+    document.getElementById("ankieta").innerHTML = this.responseText;
   }
   xhttp.open("GET", "wiercenie.html", true);
   xhttp.send();
@@ -32,8 +53,8 @@ function wiercenie_param(){
     let posuw;
     obroty = (1000*(drill_mat*drill_tool_material))/(Math.PI*drill_tool_diameter);
     posuw = drill_tool_diameter*drill_mat*1.2*drill_tool_material;
-    document.getElementById("demo2").innerHTML = Math.round(obroty);
-    document.getElementById("demo3").innerHTML = Math.round(posuw);
+    document.getElementById("wynik_obr").innerHTML = Math.round(obroty) + " obr/min";
+    document.getElementById("wynik_pos").innerHTML = Math.round(posuw) + " mm/min";
   }
   xhttp.open("GET", "wiercenie.html", true);
   xhttp.send();
@@ -43,11 +64,28 @@ function wiercenie_param(){
 function rozwiercanie(){
   const xhttp = new XMLHttpRequest();
   xhttp.onload = function() {
-    document.getElementById("demo2").innerHTML = "";
-    document.getElementById("demo3").innerHTML = "";
-    document.getElementById("demo").innerHTML = this.responseText;
+    document.getElementById("wynik_obr").innerHTML = "";
+    document.getElementById("wynik_pos").innerHTML = "";
+    document.getElementById("ankieta").innerHTML = this.responseText;
   }
   xhttp.open("GET", "rozwiercanie.html", true);
   xhttp.send();
 
+}
+
+function rozwiercanie_param() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function () {
+    let ream_mat = document.getElementById("ream_material").value;
+    let ream_tool_material = document.getElementById("ream_tool_material").value;
+    let ream_tool_diameter = document.getElementById("ream_tool_diameter").value;
+    let obroty;
+    let posuw;
+    obroty = (1000*(ream_mat*ream_tool_material))/(Math.PI*ream_tool_diameter);
+    posuw = obroty*(ream_tool_diameter*0.03);
+    document.getElementById("wynik_obr").innerHTML = Math.round(obroty) + " obr/min";
+    document.getElementById("wynik_pos").innerHTML = Math.round(posuw) + " mm/min";
+  }
+  xhttp.open("GET", "rozwiercanie.html", true);
+  xhttp.send();
 }
